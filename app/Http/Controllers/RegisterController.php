8818d54590249628b910587a934e0ed55935cf29
Ramon Ribeiro Noleto <ramon.ribeiro@inbraep.com.br>
2024-03-27 19:41:05 -0300

    fist commit

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\UserRegistered;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+
+class RegisterController extends Controller
+{
+    public function __invoke(RegisterRequest $request)
+    {
+        $input = $request->validated();
+        $user = User::query()->create($input);
+
+        UserRegistered::dispatch($user);
+        return new UserResource($user);
+    }
+}
---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h2>Seja bem vindo {{ $user->name}} </h2>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore illo odio placeat distinctio harum. Cumque reprehenderit aut tempore nesciunt possimus facere ea, dolor repudiandae vero voluptatum! Nihil itaque commodi voluptates.</p>
+</body>
+
+</html>
